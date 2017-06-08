4022e0aa3056058e92b83c34dce3469bbe038c39
FerencOnodi <onodiferenc91@gmail.com>
2017-06-08 11:18:30 +0200

    Initial commit

---
+++ src/MainDialog.java
@@ -0,0 +1,43 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+/**
+ * Created by ferenc on 2017.06.08..
+ */
+public class MainDialog {
+
+    private JFrame mainFrame;
+    private JPanel buttonPanel;
+    private JButton copyButton;
+    private JButton stopAllButton;
+
+    public MainDialog() {
+        prepareGUI();
+    }
+
+    private void prepareGUI() {
+        mainFrame = new JFrame("Multi Threaded Copy Application");
+        mainFrame.setSize(400, 500);
+        mainFrame.setLayout(new GridLayout(2, 1));
+        //Exit the application ->
+        mainFrame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+
+        //ButtonPanel -- one more panel needed before this, to display progress bars!!
+        buttonPanel = new JPanel();
+
+        mainFrame.add(buttonPanel);
+        mainFrame.setVisible(true);
+    }
+
+    public void showButton() {
+        copyButton = new JButton("Copy");
+        stopAllButton = new JButton("Stop all");
+    }
+}
